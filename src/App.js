c4afc939a27a81fdf57a776a11f3e0ec56cc156a
Rokas <rookas.rudzenskas@gmail.com>
2021-06-14 16:49:51 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="app">
         <Header />
+        <Sidebar />
     </div>
   );
 }
