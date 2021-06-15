900e3ce8104634863f743ee1c9f2fb36eb59343e
Rokas <rookas.rudzenskas@gmail.com>
2021-06-15 09:06:52 +0300

    Rokas

---
+++ src/components/Channel.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./styles/Channel.css";
+
+const Channel = ({avatar, followers, name}) => {
+    return (
+        <div className="channel">
+            <div className="channel__details">
+                <img src={avatar} alt=""/>
+                <p>{name}</p>
+            </div>
+            <p>{followers}</p>
+        </div>
+    );
+};
+
+export default Channel;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import "./styles/Sidebar.css";
+import Channel from "./Channel";
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
-            
+            <h5>FOLLOWED CHANNELS</h5>
+            <Channel avatar="https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK.jpg" name="rookasrudzianskas" followers="834K"/>
+            <Channel avatar="https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK.jpg" name="rookasrudzianskas" followers="834K"/>
+            <Channel avatar="https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK.jpg" name="rookasrudzianskas" followers="834K"/>
+            <h5>RECOMMENDED CHANNELS</h5>
+            <p className="sidebar__topShowMore">Show More</p>
         </div>
     );
 };
