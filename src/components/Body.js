ec183ccc44d094ec7e6d158500c79032e67a7834
Rokas <rookas.rudzenskas@gmail.com>
2021-06-15 09:38:44 +0300

    Rokas

---
+++ src/components/Body.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./styles/Body.css";
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="body__left">
+
+            </div>
+
+            <div className="body__rightPlaceHolder">
+
+            </div>
+        </div>
+    );
+};
+
+export default Body;
